df4fcb566edf984b0d1abdd6f7fceb278061343b
You-sen <yousufhossen5@gmail.com>
2026-03-05 23:53:38 +0600

    define job schema with mongoose

---
+++ quickserver/src/jobs/schemas/job.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type JobDocument = Job & Document;
+
+@Schema({ timestamps: true })
+export class Job {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  company: string;
+
+  @Prop({ required: true })
+  location: string;
+
+  @Prop({ required: true })
+  category: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: false })
+  salary?: string;
+
+  @Prop({ required: false })
+  type?: string; // Full-time, Part-time, Contract, etc.
+
+  @Prop({ default: true })
+  isActive: boolean;
+}
+
+export const JobSchema = SchemaFactory.createForClass(Job);
